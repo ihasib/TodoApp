cf9fa1d66177e44026f81021e9acf26c6fc6888d
S. M. Hasibur Rahman <hasibur.r@samsung.com>
2019-05-25 09:28:20 +0600

    A task adder VC class is added. keeping it at destination terminal of segue newly created task instance has been passed to sender(TodoTableViewController).

---
+++ TodoApp/TaskAdderViewController.swift
@@ -0,0 +1,55 @@
+//
+//  TaskAdderViewController.swift
+//  TodoApp
+//
+//  Created by S. M.Hasibur Rahman on 25/5/19.
+//  Copyright © 2019 S. M. Hasibur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class TaskAdderViewController: UIViewController {
+    
+    var previousVC = TodoTableViewController()
+    
+    @IBOutlet weak var taskTitleTextField: UITextField!
+    @IBOutlet weak var isImportant: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTaskTapped(_ sender: Any) {
+        //task creation
+        let task = TodoTask()
+        
+        task.isImportant = isImportant.isOn
+        if(task.isImportant){
+            task.name = "❗️"
+        }
+        
+        if let taskName = taskTitleTextField.text {
+            task.name += taskName
+        }
+        previousVC.todoTaskList.append(task)
+        previousVC.tableView.reloadData()
+    }
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TodoApp/TodoTableViewController.swift
+++ TodoApp/TodoTableViewController.swift
@@ -86,14 +86,16 @@ class TodoTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let adderVC = segue.destination as! TaskAdderViewController
+        adderVC.previousVC = self
     }
-    */
+    
 
 }
